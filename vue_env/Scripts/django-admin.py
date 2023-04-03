0eba05712d2a1246c771ed2cccdd28cec8c829b5
megharris <megharris@unomaha.edu>
2023-04-03 05:07:53 -0500

    Initial commit

---
+++ vue_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\megharris\WebstormProjects\8080A4MAH\vue_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
