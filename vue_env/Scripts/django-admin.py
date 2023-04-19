766bc6e08f57c7e866eda44c066f5b776a60c120
megharris <megharris@unomaha.edu>
2023-04-19 09:23:32 -0500

    Delete django-admin.py

--- vue_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\megharris\WebstormProjects\8080A4MAH\vue_env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
